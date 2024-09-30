b5b3c4e664ddfacf1217b6a345c0cc7b60bd1f20
RubensAugusto <rubensammiranda@gmail.com>
2024-09-30 11:03:41 -0300

    Adiciona dicionários de tokens

---
+++ src/Compilador/Gramatica/Tokens/OperadoresLogicosEnums.java
@@ -0,0 +1,10 @@
+package Compilador.Gramatica.Tokens;
+
+public enum OperadoresLogicosEnums {
+    OPERADOR_IGUAL,
+    OPERADOR_NAO_IGUAL,
+    OPERADOR_MAIOR,
+    OPERADOR_MAIOR_IGUAL,
+    OPERADOR_MENOR,
+    OPERADOR_MENOR_IGUAL
+}
---
+++ src/Compilador/Gramatica/Tokens/OperadoresMatematicosEnums.java
@@ -0,0 +1,8 @@
+package Compilador.Gramatica.Tokens;
+
+public enum OperadoresMatematicosEnums {
+    OPERADOR_ADICAO,
+    OPERADOR_SUBTRACAO,
+    OPERADOR_MULTIPLICACAO,
+    OPERADOR_DIVISAO
+}
