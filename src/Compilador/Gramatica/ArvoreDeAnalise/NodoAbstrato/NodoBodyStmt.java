98128f98d28383de28b84c41440442a63275736d
RubensAugusto <rubensammiranda@gmail.com>
2024-10-13 15:58:35 -0300

    Adiciona NodoBodyStmt

---
+++ src/Compilador/Gramatica/ArvoreDeAnalise/NodoAbstrato/NodoBodyStmt.java
@@ -0,0 +1,49 @@
+package Compilador.Gramatica.ArvoreDeAnalise.NodoAbstrato;
+
+import Compilador.Gramatica.ArvoreDeAnalise.NodoTerminal.NodoTerminal;
+import Compilador.Gramatica.Tokens.TokenEnums;
+
+public class NodoBodyStmt extends NodoAbstratoBase{
+    @Override
+    public boolean ValidarSintaxe() {
+        var filhos = RetornarNodosFilhos();
+        var tamanho = filhos.size();
+
+        if(filhos.isEmpty() ||
+            tamanho > 2)
+            return false;
+
+        if(filhos.get(0) instanceof  NodoPrototype){
+            var nodo = (NodoPrototype) filhos.get(0);
+            return nodo.ValidarSintaxe();
+        }
+
+        if (filhos.get(0) instanceof NodoAttr) {
+            var nodo = (NodoAttr) filhos.get(0);
+            return nodo.ValidarSintaxe();
+        }
+
+        if(filhos.get(0) instanceof NodoIf) {
+            var nodo = (NodoIf) filhos.get(0);
+            return nodo.ValidarSintaxe();
+        }
+
+        if(filhos.get(0) instanceof NodoMethodCall){
+            var nodo = (NodoMethodCall) filhos(0);
+            return nodo.ValidarSintaxe();
+        }
+
+        if(tamanho == 2){
+            if(!(filhos.get(0) instanceof NodoTerminal))
+                return false;
+
+            var nodoTerminal = (NodoTerminal) filhos.get(0);
+            if(nodoTerminal.RetornarTipo() !=  TokenEnums.PALAVRA_CHAVE_RETURN)
+                return false;
+
+            return filhos.get(1) instanceof NodoName;
+        }
+
+        return true;
+    }
+}
