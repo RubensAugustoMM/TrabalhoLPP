6781794e2a100ccc2da90b2c4a018ad700760fca
RubensAugustoMM <rubensammiranda@gmail.com>
2024-10-17 17:25:48 -0300

    Adiciona coisas

--- src/Compilador/AnalizadorLexico/AnalizadorLexico.java
+++ src/Compilador/AnalizadorLexico/AnalizadorLexico.java
@@ -1,14 +1,22 @@
 package Compilador.AnalizadorLexico;
 
+import Compilador.Gramatica.ArvoreDeAnalise.NodoAbstrato.NodoProgram;
 import Compilador.Gramatica.ArvoreDeAnalise.NodoBase;
 
 import java.util.Stack;
 
 public class AnalizadorLexico {
     private Stack<NodoBase> _pilha = new Stack<>();
+    private NodoBase _arvore;
+    private NodoBase _nodoAtual;
+    private NodoBase _nodoAnterior;
 
     public Stack<NodoBase> RetornarPilha(){
         return _pilha;
+    }
+
+    public NodoBase RetornarArvore(){
+        return _arvore;
     }
 
     public void AnalizarToken(){
@@ -16,18 +24,18 @@ public class AnalizadorLexico {
     }
 
     public void AnalizarCodigo(String codigo){
+        _arvore = new NodoProgram();
         for(int i = 0; VerificarFimDoArquivo(codigo,i);i++){
              char charactere = codigo.charAt(i);
 
              if(charactere == ' '){
                  continue;
              }
-
-             if(charactere == ){}
+             if(charactere == ''){}
         }
     }
 
     private boolean VerificarFimDoArquivo(String codigo, int i){
-        return codigo.length() <= i;
+        return codigo.length() < i;
     }
 }
